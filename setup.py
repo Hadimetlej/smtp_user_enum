81dfdedda15f7b53f77add10a9684368336bc0b5
Hadimetlej <hadi531metlej@gmail.com>
2025-03-21 22:43:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import setup, find_packages # type: ignore
+# filepath: c:\Users\hadi5\OneDrive\Desktop\Machine learning\setup.py
+from setuptools import setup, find_packages
 
 setup(
     name="smtp_user_enum",  # Name of your project
     version="1.0.0",  # Version of the project
     description="SMTP User Enumeration Script for enumerating valid usernames on an SMTP server",
-    author="Hadi Metlej",  
-    author_email="hadi531metlej@gmai.com",
-    url="https://github.com/Hadimetlej/smtp_user_enum",  # Project URL (replace with your GitHub or website URL)
-    packages=find_packages(),  
-    install_requires=[  
-        "colorama",  
+    author="Hadi Metlej",
+    author_email="hadi531metlej@gmail.com",
+    url="https://github.com/Hadimetlej/smtp_user_enum",  # Project URL
+    packages=find_packages(),
+    install_requires=[
+        "colorama==0.4.4",
+        "requests>=2.25.0",
     ],
-    entry_points={  # Create a command-line script entry point
+    entry_points={
         'console_scripts': [
-            'smtp_user_enum = smtp_user_enum:main',  # Replace with the function name to run the script
+            'smtp_user_enum = smtp_user_enum.smtp_user_enum:main',  # Entry point for the script
         ],
     },
-    classifiers=[  # Optional: Some classifiers that give metadata about the package
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Minimum Python version required
+    python_requires='>=3.6',
 )
